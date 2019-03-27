ae73dde7b11659226b0d571037dc6a75886d8bd9
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-03-27 01:54:00 -0500

    pushing WiP

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 //array of objects with information about students
 const students = [];
 const houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'];
+const houseCrests = [{
+        houseName: 'Gryffindor',
+        imageUrl: 'https://vignette.wikia.nocookie.net/pottermore/images/1/16/Gryffindor_crest.png'
+    },
+    {
+        houseName: 'Hufflepuff',
+        imageUrl: 'https://vignette.wikia.nocookie.net/pottermore/images/5/5e/Hufflepuff_crest.png'
+    },
+    {
+        houseName: 'Ravenclaw',
+        imageUrl: 'https://vignette.wikia.nocookie.net/pottermore/images/4/40/Ravenclaw_Crest_1.png'
+    },
+    {
+        houseName: 'Slytherin',
+        imageUrl: 'https://vignette.wikia.nocookie.net/pottermore/images/4/45/Slytherin_Crest.png'
+    }
+];
 
 
 //print function which prints the designated variable to the designated element on the page
@@ -10,16 +27,22 @@ const printToDom = (divId, textToPrint) => {
 };
 
 //Function which builds objects to go in the students array
-const studentObjectBuilder = (name, house, expButtonId, cardClass, buttonClass, buttonText) => {
+const studentObjectBuilder = (name, house, expButtonId, buttonClass, buttonText) => {
     let expelButtonCounter = 0;
     const student = {
         name: name,
         house: house,
         expeled: false,
         expButtonId: `${expButtonId}${expelButtonCounter}`,
-        cardClass: cardClass,
-        buttonClass: buttonClass
-    }
+        cardClass: `${house.toLowerCase()}-card`,
+        buttonClass: buttonClass,
+        buttonText: buttonText,
+        houseCrests.forEach((Crest) => {
+            if (Crest.houseName === house) {
+                imageUrl: Crest.imageUrl;
+            };
+        }),
+    };
     students.push(student);
     expelButtonCounter++;
 };
@@ -58,19 +81,19 @@ const domStringBuilder = (arr) => {
     let domString = '';
     domString += `<div class='container'>`;
     domString += `<div class='row'>`;
-    domString += `<div class='col-sm-12 col-md-6 col-lg-4'>`;
 
     arr.forEach((student) => {
+        domString += `<div class='col-sm-12 col-md-6 col-lg-4'>`;
         domString += `<div class="card">`;
-        domString += `<img src="..." class="card-img-top" alt="...">`;
+        domString += `<img src="${student.imageUrl}" class="card-img-top" alt="Image of the ${student.house} crest">`;
         domString += `<div class="${student.cardClass} card-body">`;
         domString += `<h5 class="card-title">${student.name}</h5>`;
         domString += `<p class="card-text">House: ${student.house}</p>`;
         domString += `<button type="button" class="${student.buttonClass} btn btn-secondary btn-sm" id='${student.expButtonId}'>${student.buttonText}</button>`;
         domString += `</div>`;
         domString += `</div>`;
+        domString += `</div>`;
     })
-    domString += `</div>`;
     domString += `</div>`;
     domString += `</div>`;
     printToDom('studentCardContainer', domString);
@@ -88,9 +111,8 @@ const sortButtonClick = () => {
     if (document.getElementById('studentNameInput').value !== '') {
         const studentName = document.getElementById('studentNameInput').value;
         let studentHouse = houses[randomNumGenerator()];
-        let studentCardClass = `${studentHouse.toLowerCase()}-card`;
         hideForm();
-        studentObjectBuilder(studentName, studentHouse, 'expelButton', studentCardClass, 'expel-button', 'Expel From Hogwarts');
+        studentObjectBuilder(studentName, studentHouse, 'expelButton', 'expel-button', 'Expel From Hogwarts');
         domStringBuilder(students);
         console.log(students[0].house);
         clearInput('studentNameInput');
@@ -110,12 +132,12 @@ const buttonListener = () => {
 };
 
 const init = () => {
-    let studentHouse = houses[randomNumGenerator()];
-    let studentCardClass = `${studentHouse.toLowerCase()}-card`;
+
+
     hideForm();
-    studentObjectBuilder('Hagrid', studentHouse, 'expelButton', studentCardClass, 'expel-button', 'Expel From Hogwarts');
-    studentObjectBuilder('Ron', studentHouse, 'expelButton', studentCardClass, 'expel-button', 'Expel From Hogwarts');
-    studentObjectBuilder('Newt', studentHouse, 'expelButton', studentCardClass, 'expel-button', 'Expel From Hogwarts');
+    studentObjectBuilder('Hagrid', houses[randomNumGenerator()], 'expelButton', 'expel-button', 'Expel From Hogwarts');
+    studentObjectBuilder('Ron', houses[randomNumGenerator()], 'expelButton', 'expel-button', 'Expel From Hogwarts');
+    studentObjectBuilder('Newt', houses[randomNumGenerator()], 'expelButton', 'expel-button', 'Expel From Hogwarts');
     buttonListener();
     domStringBuilder(students);
 };
